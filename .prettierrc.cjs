49b81277f8eb81884b9bb90ff7e5b826ab913c6e
Skyclo <hello.skyclo@gmail.com>
2023-01-17 17:18:49 -0500

    📦 add prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    semi: false,
+    singleQuote: false,
+    quoteProps: "as-needed",
+    jsxSingleQuote: false,
+    trailingComma: "es5",
+    bracketSpacing: true,
+    bracketSameLine: true,
+    arrowParens: "avoid",
+    requirePragma: false,
+    insertPragma: false,
+    proseWrap: "preserve",
+    endOfLine: "lf",
+    plugins: [],
+}
