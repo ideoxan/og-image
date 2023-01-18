e0dccdb5c78273089ff76ffcb1e1a52a81912878
Skyclo <hello.skyclo@gmail.com>
2023-01-18 00:08:37 -0500

    🎨 improve structure, fix types

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import { generate } from "ezog/cloudflare"
 
 export interface Env {} // KV and durable objects
 
-const rFetch = async (url: string): Promise<ArrayBuffer> =>
-    await fetch(url).then(async res => await res.arrayBuffer())
+// Constants
+const BASE_URL: string = "https://ideoxan.com/"
+const CACHE_TTL: number = 60 * 60 * 24 * 7 // 1 week
 
-const baseUrl: string = "https://ideoxan.com/"
+// Util Methods
+const arrayBufferFetch = async (url: string): Promise<ArrayBuffer> =>
+    await fetch(url).then(async (res: Response): Promise<ArrayBuffer> => await res.arrayBuffer())
 
+// Export
 export default {
     async fetch(request: Response, env: Env, ctx: ExecutionContext): Promise<Response> {
         let url: URL = new URL(request.url)
@@ -21,84 +25,90 @@ export default {
             if (cachedResponse) return cachedResponse
         } else await cache.delete(url)
 
-        let logo: Promise<ArrayBuffer> = rFetch(`${baseUrl}images/ix_logo_white_trans_253x50.png`)
-        let background: Promise<ArrayBuffer> = rFetch(`${baseUrl}images/og_bg.png`)
+        let logoImage: Promise<ArrayBuffer> = arrayBufferFetch(
+            `${BASE_URL}images/ix_logo_white_trans_253x50.png`
+        )
+        let backgroundImage: Promise<ArrayBuffer> = arrayBufferFetch(`${BASE_URL}images/og_bg.png`)
 
-        const png: Uint8Array = await generate(
-            [
-                {
-                    type: "image",
-                    buffer: await background,
-                    x: 0,
-                    y: 0,
-                    width: 800,
-                    height: 400,
-                },
-                {
-                    type: "image",
-                    buffer: await logo,
-                    x: 64,
-                    y: 140,
-                    width: 76,
-                    height: 15,
-                },
-                {
-                    type: "textBox",
-                    text: url.searchParams.get("lname") || "ideoxan.com",
-                    x: 64,
-                    y: 180,
-                    width: 676,
-                    fontFamily: ["Inter 800"],
-                    fontSize: 32,
-                    lineHeight: 34,
-                    align: "left",
-                    color: "#F8F5FF",
-                },
-                {
-                    type: "textBox",
-                    text: url.searchParams.get("desc") || "Sample description",
-                    x: 64,
-                    y: 230,
-                    width: 676,
-                    fontFamily: ["Inter 400"],
-                    fontSize: 14,
-                    lineHeight: 16,
-                    align: "left",
-                    color: "#c8c4d2",
-                },
-            ],
-            {
-                width: 800,
-                height: 400,
-                fonts: [
+        let response: Response = new Response(
+            await generate(
+                // Layers
+                [
                     {
-                        type: "googleFont",
-                        name: "Inter 800",
-                        googleFontName: "Inter",
-                        weight: 800,
+                        type: "image",
+                        buffer: await backgroundImage,
+                        x: 0,
+                        y: 0,
+                        width: 800,
+                        height: 400,
                     },
                     {
-                        type: "googleFont",
-                        name: "Inter 600",
-                        googleFontName: "Inter",
-                        weight: 600,
+                        type: "image",
+                        buffer: await logoImage,
+                        x: 64,
+                        y: 140,
+                        width: 76,
+                        height: 15,
                     },
                     {
-                        type: "googleFont",
-                        name: "Inter 400",
-                        googleFontName: "Inter",
-                        weight: 400,
+                        type: "textBox",
+                        text: url.searchParams.get("lname") || "ideoxan.com",
+                        x: 64,
+                        y: 180,
+                        width: 676,
+                        fontFamily: ["Inter 800"],
+                        fontSize: 32,
+                        lineHeight: 34,
+                        align: "left",
+                        color: "#F8F5FF",
+                    },
+                    {
+                        type: "textBox",
+                        text: url.searchParams.get("desc") || "Sample description",
+                        x: 64,
+                        y: 230,
+                        width: 676,
+                        fontFamily: ["Inter 400"],
+                        fontSize: 14,
+                        lineHeight: 16,
+                        align: "left",
+                        color: "#c8c4d2",
                     },
                 ],
-                background: "#fff",
+                // Overall image settings
+                {
+                    width: 800,
+                    height: 400,
+                    fonts: [
+                        {
+                            type: "googleFont",
+                            name: "Inter 800",
+                            googleFontName: "Inter",
+                            weight: 800,
+                        },
+                        {
+                            type: "googleFont",
+                            name: "Inter 600",
+                            googleFontName: "Inter",
+                            weight: 600,
+                        },
+                        {
+                            type: "googleFont",
+                            name: "Inter 400",
+                            googleFontName: "Inter",
+                            weight: 400,
+                        },
+                    ],
+                    background: "#fff",
+                }
+            ),
+            {
+                headers: {
+                    "Content-Type": "image/png",
+                    "Cache-Control": `s-maxage=${CACHE_TTL}`,
+                },
             }
         )
-        let response: Response = new Response(png, {
-            headers: {
-                "Content-Type": "image/png",
-                "Cache-Control": "s-maxage=604800",
-            },
-        })
 
         // Cache the response
         await cache.put(url, response.clone())
